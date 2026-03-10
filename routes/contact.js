464a09c807ff0e1983b8009d31c9e5357ccd876a
Mohammad Zakriya <mzakriya837@gmail.com>
2026-03-10 11:18:32 +0500

    Delete routes directory

--- routes/contact.js
+++
@@ -1,9 +0,0 @@
-// routes/contact.js
-const router = require('express').Router();
-const ctrl   = require('../controllers/contactController');
-const { contactRules } = require('../middleware/validate');
-
-router.post('/', contactRules, ctrl.createContact);
-router.get ('/',               ctrl.getAllContacts);
-
-module.exports = router;
